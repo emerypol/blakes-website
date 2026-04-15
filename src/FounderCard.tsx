df6c21e8bd4a04d084e8f4bfd9b67b5d4762196f
emery pollack <emerypol@umich.edu>
2026-04-14 21:59:48 -0600

    aboutus now done to latest updates

--- src/FounderCard.tsx
+++ src/FounderCard.tsx
@@ -1,24 +1,29 @@
-import twitter from "/src/assets/twitter.png";
 
-function FounderCard() {
+
+function FounderCard({ img, name, role, desc }: {
+    img: string;
+    name: string;
+    role: string;
+    desc: string;
+}) {
     return (
         <div className="flex justify-center">
             <div className="max-w-[424px] sm:max-w-[620px] w-[90vw]">
                 <div className="flex flex-col sm:flex-row  rounded-2xl shadow-lg p-6 border border-[#e2e2e2] bg-white
                                 justify-center items-center sm:items-start">
-                    <img src={twitter}
+                    <img src={img}
                         className="h-[120px] w-[120px] object-cover rounded-2xl" />
-                    <div className="flex flex-col justify-center sm:justify-start px-5">
+                    <div className="flex flex-col justify-center sm:justify-start ps-5">
                         <div className="text-[rgba(11,93,185,0.9)] text-[clamp(5px,8vw,34px)] md:text-[34px] font-semibold
                                         text-center sm:text-start pt-3 sm:pt-0">
-                            Blake Veltema
+                            {name}
                         </div>
                         <div className="text-white bg-[rgb(120,180,248)] text-center sm:text-start 
                                         mb-2 ps-3 rounded-lg font-bold">
-                            Founder & Owner
+                            {role}
                         </div>
                         <div className=" text-center sm:text-start">
-                            Blake grew up from 0 years old into an adult. He currently lives in west michigan. He founded WCI in 2010. He's been doing this for over 10 years. Other sentences needed.
+                            {desc}
                         </div>
                     </div>
                 </div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,6 +4,7 @@ import FounderCard from "../FounderCard";
 import NavAndLanding from "../NavAndLanding";
 import WhyChooseUs from "../WhyChooseUs";
 import bgdk from "/src/assets/blake_we_2_desktop.jpg";
+import twitter from "/src/assets/twitter.png";
 
 function About() {
     return (
@@ -12,13 +13,24 @@ function About() {
 
 
             <div className={` bg-[rgba(255, 255, 255, 0.8)] bg-cover`}>
-
+                <div className="w-full mt-2" ></div>
                 <AboutCards />
 
-                <WhyChooseUs />
+                <div className="w-full mt-6" ></div>
 
-                <FounderCard />
-                <div className="w-full mt-12" ></div>
+                <WhyChooseUs />
+                <div className="w-full mt-16" ></div>
+                <div className="flex justify-center">
+                    <div className="w-[90vw] max-w-[1320px] grid grid-cols-1 lg:grid-cols-2 gap-6">
+                        <FounderCard img={twitter} name="Blake" role="Founder & Owner"
+                            desc="a to one word words a to one word a one to a words one to word a to one word words a to one word a one to a words one to word a to one word words a to one word a one to a words one to word a "
+                        />
+                        <FounderCard img={twitter} name="Mallory" role="Founder & Owner"
+                            desc="a to one word words a to one word a one to a words one to word a to one word words a to one word a one to a words one to word a to one word words a to one word a one to a words one to word a "
+                        />
+                    </div>
+                </div>
+                <div className="w-full mt-16" ></div>
 
             </div>
 
